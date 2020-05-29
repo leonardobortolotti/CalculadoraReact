ba5cf634a241efdeb6fd3279a83ad7e794f5e886
Leonardo Bortolotti <leonardomarques343@gmail.com>
2020-05-29 09:06:38 -0300

    correcao no css

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import './Button.css'
 
 export default props => {
-    let classes = 'button'
-    classes += props.operation ? 'operation' : '';
-    classes += props.double ? 'double' : '';
-    classes += props.triple ? 'triple' : '';
+    let classes = 'button '
+    classes += props.operation ? ' operation' : '';
+    classes += props.double ? ' double' : '';
+    classes += props.triple ? ' triple' : '';
 
 
     return <button 
