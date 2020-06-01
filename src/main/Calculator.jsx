0c24897c00cd70d24383840083850e4e795885e5
Leonardo Bortolotti <leonardomarques343@gmail.com>
2020-06-01 08:53:19 -0300

    finalizacao da logica

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -29,7 +29,29 @@ export default class Calculator extends Component {
     }
 
     setOperation(operation) {
+        if (this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true})
+        }
 
+        const equals = operation === '='
+        const currentOperation = this.state.operation
+
+        const values = [...this.state.values]
+
+        try {
+            values[0] = eval(`${(values[0])} ${currentOperation} ${values[1]}`)
+            values[1] = 0
+        } catch(e) {
+            values[0] = this.state.values[0]
+        }
+
+        this.setState({
+            displayValue: values[0],
+            operation: equals ? null : operation,
+            current: equals ? 0 : 1,
+            clearDisplay: !equals,
+            values
+        })
     }
 
     addDigit(n) {
